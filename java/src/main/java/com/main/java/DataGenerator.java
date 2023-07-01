d3e968ae5d65731f0ed383a659ac44c761f4fe85
Jacob Marchywka <marchywk@umich.edu>
2023-07-01 15:37:08 -0400

    Pushing new files

--- java/src/main/java/com/main/java/DataGenerator.java
+++ java/src/main/java/com/main/java/DataGenerator.java
@@ -1,13 +1,49 @@
 package com.main.java;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
 public class DataGenerator {
-    public void createData(int size) {
+
+    private int size;
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Bean
+    public int readIn() throws IOException {
+        String fileName = "path/to/your/file.txt"; // Specify the path to your text file
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            size = Integer.parseInt(reader.readLine());
+            return size;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Autowired
+    public void createData(int n) {
 
         Random random = new Random();
-        for (int i = 0; i < size; i++) {
-            System.out.println(random.nextInt(size));
+        for (int i = 0; i < n; i++) {
+            System.out.println(random.nextInt(n));
         }
     }
 }
--- java/src/main/java/com/main/java/JavaApplication.java
+++ java/src/main/java/com/main/java/JavaApplication.java
@@ -1,5 +1,6 @@
 package com.main.java;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
